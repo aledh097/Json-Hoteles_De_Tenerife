2a8a2723ebcf53bba83fc8a2a9d57b8bc604e2e1
Alejandro Palomino García <muchoh123@hotmail.com>
2016-02-22 12:05:00 +0100

    Apartado 4 completado

--- hoteles.py
+++ hoteles.py
@@ -1,10 +1,18 @@
-# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
 import json
 import ast
 print "------------------------------------------"
 print "A= LISTAR NOMBRE DE LOS HOTELES JUNTO A SU COORDENADA"
 print "B= NUMERO DE HOTELES EN EL FICHERO JSON"
 print "C= BUSCAR DIRECCION SEGUN NOMBRE DE HOTEL"
+print "D= BUSCAR HOTELES SEGUN SU TIPO"
+
 print "------------------------------------------"
 tecla=raw_input("Opcion: ")
 
@@ -23,7 +31,7 @@ if tecla=="b" or tecla=="B":
 	contador=0
 	for l in doc:
 		contador=contador+1
-	print u"Número de hoteles: %s" % (contador)
+	print "Numero de hoteles: %s" % (contador)
 
 if tecla=="c" or tecla=="C":
 	buscador=raw_input("Dime el nombre del hotel: ")
@@ -33,3 +41,12 @@ if tecla=="c" or tecla=="C":
 	for l in doc:
 		if l["NOMBRE"]==buscador:
 			print l["DIRECCION"]
+
+if tecla=="d" or tecla=="D":
+	buscador=raw_input("Dime el tipo de hotel: ")
+	fichero=open("/home/usuario/prueba/prueba/hoteles.json","r")
+	lineas=json.loads(fichero.readline())
+	doc=lineas["docs"]
+	for l in doc:
+		if unicode(l["TIPO"])==unicode(buscador):
+			print l["NOMBRE"]
